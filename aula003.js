81fedc1a99615850ba9ada6683c47b3b027edf81
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-03-22 13:09:05 -0300

    diferenças entre var let e const

---
+++ aula003.js
@@ -0,0 +1,21 @@
+"use strict"
+var nome = "bruno"
+console.log("nome:" + nome)
+if(true){
+    let nome = "bruno"
+    console.log("nome do user:" + nome)
+}
+function teste(){
+    let nome = "bruno"
+    if(true){
+        console.log("nome (d.e):" + nome)
+    }
+    console.log("nome (fuction):" + nome)
+}
+let word ="hello"
+word = "oi"
+console.log(word)
+
+const idade = 10
+idade = 10 //dará erro no programa
+console.log(idade)
