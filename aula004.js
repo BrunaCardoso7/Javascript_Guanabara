57abd12928956930f90ede9f59892278c28735ad
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-03-22 22:45:52 -0300

    operadores relacionais

---
+++ aula004.js
@@ -0,0 +1,16 @@
+let num1=10, num2=10, res=0
+num1=10
+num2= 2
+num++//incremento
+res = num1-num2
+console.log(res)
+console.log(num1)
+console.log(num2)
+console.log(num1 + num2)
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1/num2)
+console.log(num1*num2)
+console.log(num1%num2)
+
+
---
+++ aula005.js
@@ -0,0 +1,8 @@
+let num1= 10
+let num2 = 15
+let num3 = 10
+res = num1 > num2
+console.log(num1 > num2)//false
+console.log(num1 < num2)//true
+console.log(num1 >= num3)//true
+console.log(res)//false
