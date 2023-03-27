a5993786055f70f7fef7c75907aadf6b4a203a1b
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-03-27 00:25:14 -0300

    atualização de estudos

---
+++ aula009.js
@@ -0,0 +1,8 @@
+var vel = 50
+if(vel <= 60){
+    console.log(`A velocidade do se carro é igual a ${vel} por hr`)
+    console.log('dirija usando cinto de segurança')
+}else{
+    console.log(`A velocidade do se carro é igual a ${vel} por hr`)
+    console.log('diminua a velocidade pelo bem da sua segurança')
+}
