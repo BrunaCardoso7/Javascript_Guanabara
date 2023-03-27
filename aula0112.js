19604302a5c080331a495f6c17a2b9b972482e3c
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-03-27 13:43:44 -0300

    voto

---
+++ aula0112.js
@@ -0,0 +1,33 @@
+var agora = new Date()
+var dia_sem = agora.getDay()
+/*  0-domingo
+    1-segunda
+    2-terça
+    3-quarta
+    4-quinta
+    5-sexta
+    6-sábado
+*/
+switch(dia_sem){
+    case 0:
+        console.log('domingo')
+        braek
+    case 1:
+        console.log('segunda')
+        braek
+    case 2:
+        console.log('terça')
+        braek
+    case 3: 
+        console.log('quarta')
+        braek
+    case 4: 
+        console.log('quinta')
+        braek
+    case 5: 
+        console.log('sexta')
+        braek
+    case 6: 
+        console.log('sábado')
+        braek
+}
