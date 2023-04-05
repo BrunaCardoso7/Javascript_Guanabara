ef3bd8ac43881bd73bce9f21cbd55b662a9dbfed
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-04 23:22:22 -0300

    consegui ajeitar o bug

--- exc01/001/exc001.js
+++ exc01/001/exc001.js
@@ -1,7 +1,5 @@
 function carregar(){
     var msg = document.getElementById('text')
-    var img = document.getElementbyId('imagem') 
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `São exatamente ${hora} horas`
+    var data = new Date
+    msg.innerHTML = `São exatamente ` + data.getHours() + `horas`
 }
