7a7458e95574e05b11dce206b84225f71241e563
Bruna Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-04-09 22:59:48 -0300

    finalização do exercício

--- exc 3 javasvript/exercicio.js
+++ exc 3 javasvript/exercicio.js
@@ -9,12 +9,21 @@ function verificar(){
         var sex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        //criando uma tag html dinâmica
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (sex[0].checked) {
             genero = 'Homem'
+            img.setAttribute('src', 'zenitsu.jpg')
+            
         }else if (sex[1].checked){
             genero = 'Mulher'
+            img.setAttribute('src', 'recoverygirl.jpg')
         } 
-        resp.innerHTML = `O usuario é ${genero}, e tem ${idade} idade`
+        resp.style.textAlign = 'center'
+        resp.innerHTML = `O usuario é ${genero}, e tem ${idade} anos de idade`
+        resp.appendChild(img)
+        
     }
 
 }
